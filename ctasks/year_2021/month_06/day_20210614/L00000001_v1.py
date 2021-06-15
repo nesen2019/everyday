7e81a3d36bd5b78aef445fbe12b7699fb58856d3
nesen2019 <228730267@qq.com>
2021-06-15 23:50:24 +0800

    a

--- ctasks/year_2021/month_06/day_20210614/L00000001_v1.py
+++ ctasks/year_2021/month_06/day_20210614/L00000001_v1.py
@@ -47,14 +47,8 @@ from clecode import decorator_default
 def ctest(method_name, class_name):
     return f"""
     
-    >>> res = {class_name}().{method_name}([2,7,11,15], 9)
-    >>> res in [[0, 1], [1,0]]
-    True
-
-    >>> res = {class_name}().{method_name}([3,2,4], 6)
-    >>> res in [[1,2], [2,1]]
-    True
-
+    >>> 
+    >>> res = {class_name}().{method_name}()
     """
 
 
