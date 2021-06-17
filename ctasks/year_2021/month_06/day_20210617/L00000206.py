23277385c1b23efeacb69ed82cd3f4de31067487
nesen2019 <228730267@qq.com>
2021-06-17 09:23:23 +0800

    c

---
+++ ctasks/year_2021/month_06/day_20210617/L00000206.py
@@ -0,0 +1,76 @@
+'''
+206.Reverse Linked List
+206.反转链表
+
+https://leetcode-cn.com/problems/
+reverse-linked-list
+
+给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+
+
+ 
+示例 1：
+
+
+输入：head = [1,2,3,4,5]
+输出：[5,4,3,2,1]
+
+示例 2：
+
+
+输入：head = [1,2]
+输出：[2,1]
+
+示例 3：
+
+输入：head = []
+输出：[]
+
+ 
+提示：
+
+链表中节点的数目范围是 [0, 5000]
+-5000 <= Node.val <= 5000
+
+ 
+进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
+
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+'''
+
+from typing import List
+from clecode.structures import ListNode, HandleLink
+from clecode import decorator_default
+
+
+@decorator_default("")
+def ctest(method_name, class_name):  
+    return f"""
+    
+    >>> 
+    >>> res = {class_name}().{method_name}()
+    """
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        pass
+
+
+if __name__ == "__main__":  
+    import doctest  
+    
+    doctest.testmod()
