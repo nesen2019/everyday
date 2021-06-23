f0ad43e5e1165e714435b7de3c2c318aab5ef39f
nesen2019 <228730267@qq.com>
2021-06-23 21:34:29 +0800

    c

---
+++ ctasks/year_2021/month_06/interview_questions/L01000029.py
@@ -0,0 +1,67 @@
+'''
+面试题 17.21.Volume of Histogram LCCI
+面试题 17.21.直方图的水量
+
+https://leetcode-cn.com/problems/
+volume-of-histogram-lcci
+
+给定一个直方图(也称柱状图)，假设有人从上面源源不断地倒水，最后直方图能存多少水量?直方图的宽度为 1。
+
+上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的直方图，在这种情况下，可以接 6 个单位的水（蓝色部分表示水）。 感谢 Marcos 贡献此图。
+示例:
+输入: [0,1,0,2,1,0,1,3,2,1,2,1]
+输出: 6
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+'''
+
+"""
+- 动态规划
+- 单调栈
+- 双指针
+"""
+
+from typing import List
+from clecode import decorator_default
+
+
+@decorator_default("")
+def ctest(method_name, class_name):
+    return f"""
+    
+    >>> 
+    >>> {class_name}().{method_name}([0,1,0,2,1,0,1,3,2,1,2,1])
+    6
+    """
+
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
+
+        n = len(height)
+        leftMax = [height[0]] + [0] * (n - 1)
+        for i in range(1, n):
+            leftMax[i] = max(leftMax[i - 1], height[i])
+
+        rightMax = [0] * (n - 1) + [height[n - 1]]
+        for i in range(n - 2, -1, -1):
+            rightMax[i] = max(rightMax[i + 1], height[i])
+
+        ans = sum(min(leftMax[i], rightMax[i]) - height[i] for i in range(n))
+        return ans
+
+
+# 作者：LeetCode-Solution
+# 链接：https://leetcode-cn.com/problems/volume-of-histogram-lcci/solution/zhi-fang-tu-de-shui-liang-by-leetcode-so-7rla/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+Solution.trap.__doc__ = ctest("trap", "Solution")
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
