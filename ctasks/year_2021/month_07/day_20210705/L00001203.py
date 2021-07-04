1d7a11c0f18f223317cb39269da1fc59bd2f2da7
nesen2019 <228730267@qq.com>
2021-07-04 23:44:47 +0800

    c

---
+++ ctasks/year_2021/month_07/day_20210705/L00001203.py
@@ -0,0 +1,125 @@
+'''
+1114.Print in Order
+1114.按序打印
+
+https://leetcode-cn.com/problems/
+print-in-order
+
+我们提供了一个类：
+
+public class Foo {
+  public void first() { print("first"); }
+  public void second() { print("second"); }
+  public void third() { print("third"); }
+}
+三个不同的线程 A、B、C 将会共用一个 Foo 实例。
+
+一个将会调用 first() 方法
+一个将会调用 second() 方法
+还有一个将会调用 third() 方法
+
+请设计修改程序，以确保 second() 方法在 first() 方法之后被执行，third() 方法在 second() 方法之后被执行。
+ 
+示例 1:
+
+输入: [1,2,3]
+输出: "firstsecondthird"
+解释: 
+有三个线程会被异步启动。
+输入 [1,2,3] 表示线程 A 将会调用 first() 方法，线程 B 将会调用 second() 方法，线程 C 将会调用 third() 方法。
+正确的输出是 "firstsecondthird"。
+
+示例 2:
+
+输入: [1,3,2]
+输出: "firstsecondthird"
+解释: 
+输入 [1,3,2] 表示线程 A 将会调用 first() 方法，线程 B 将会调用 third() 方法，线程 C 将会调用 second() 方法。
+正确的输出是 "firstsecondthird"。
+ 
+提示：
+
+尽管输入中的数字似乎暗示了顺序，但是我们并不保证线程在操作系统中的调度顺序。
+你看到的输入格式主要是为了确保测试的全面性。
+
+
+class Foo:
+    def __init__(self):
+        pass
+
+
+    def first(self, printFirst: 'Callable[[], None]') -> None:
+        
+        # printFirst() outputs "first". Do not change or remove this line.
+        printFirst()
+
+
+    def second(self, printSecond: 'Callable[[], None]') -> None:
+        
+        # printSecond() outputs "second". Do not change or remove this line.
+        printSecond()
+
+
+    def third(self, printThird: 'Callable[[], None]') -> None:
+        
+        # printThird() outputs "third". Do not change or remove this line.
+        printThird()
+'''
+
+from clecode import decorator_default
+
+from typing import Callable
+
+
+@decorator_default("")
+def ctest(method_name, class_name):
+    return f"""
+    
+    >>> 
+    >>> print("This problem requires human judgment.")
+    """
+
+
+from threading import Lock
+
+
+class Foo:
+    def __init__(self):
+        self.firstJobDone = Lock()
+        self.secondJobDone = Lock()
+        self.firstJobDone.acquire()
+        self.secondJobDone.acquire()
+
+    def first(self, printFirst: 'Callable[[], None]') -> None:
+        # printFirst() outputs "first".
+        printFirst()
+        # Notify the thread that is waiting for the first job to be done.
+        self.firstJobDone.release()
+
+    def second(self, printSecond: 'Callable[[], None]') -> None:
+        # Wait for the first job to be done
+        with self.firstJobDone:
+            # printSecond() outputs "second".
+            printSecond()
+            # Notify the thread that is waiting for the second job to be done.
+            self.secondJobDone.release()
+
+    def third(self, printThird: 'Callable[[], None]') -> None:
+        # Wait for the second job to be done.
+        with self.secondJobDone:
+            # printThird() outputs "third".
+            printThird()
+
+
+# 作者：LeetCode
+# 链接：https://leetcode-cn.com/problems/print-in-order/solution/an-xu-da-yin-by-leetcode/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+Solution = Foo
+Solution.__doc__ = ctest()
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
